0faaa49cccdf59dd47be2290e3e401b23960d8d7
Amal Krishna <149648403+Amal-Krishna-TJ@users.noreply.github.com>
2024-08-01 23:13:13 +0530

    Updated images

--- src/app/units/footer/footer.component.ts
+++ src/app/units/footer/footer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ContactusComponent } from '../../pages/contactus/contactus.component';
 import { DepartmentComponent } from '../../pages/department/department.component';
 import { ServicesComponent } from '../../pages/services/services.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ContactusComponent,DepartmentComponent,ServicesComponent],
+  imports: [ContactusComponent,DepartmentComponent,ServicesComponent,RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
